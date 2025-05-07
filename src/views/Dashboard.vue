862ea3234aa262453544c31767de9e3c321fff1f
milo <milo.grv@gmail.com>
2025-05-07 19:33:31 +0200

    avatar url fix

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,9 @@
 <template>
-  <div v-if="users">
+  <div v-if="user">
     <h1>Salut @{{ user?.globalName }} :)</h1>
     <p>Coins : {{ user?.coins }}</p>
     <p>Warns : {{ user?.allTimeWarns }}</p>
-    <img :src="getAvatar(discordId)" alt="avatar" />
+    <img :src="getAvatar" alt="avatar" />
     <!-- Add your user-specific content here -->
     <button @click="logout">Logout</button>
   </div>
@@ -15,14 +15,33 @@ import axios from 'axios'
 export default {
   computed: {
     user() {
+      const filtered = this.users?.filter(u => u.id === this.discordId)
+      if (filtered.length < 1) return null
       return this.users?.filter(u => u.id === this.discordId)[0]
+    },
+    getAvatar() {
+      const fetchUrl = import.meta.env.VITE_FLAPI_URL + '/user/' + this.discordId + '/avatar'
+      try {
+        console.log(fetchUrl)
+        const response = axios.get(fetchUrl, {
+          headers: {
+            'ngrok-skip-browser-warning': 'true',
+            'Content-Type': 'application/json'
+          },
+          withCredentials: false
+        }).then((response) => {
+          console.log(response.data.avatarUrl)
+          return response.data.avatarUrl
+        })
+      } catch (e) {
+        console.error('flAPI error:', e)
+      }
     }
   },
   data() {
     return {
       discordId: null,
       users: null,
-      avatar: null,
     }
   },
   async mounted() {
@@ -51,23 +70,6 @@ export default {
         console.error('flAPI error:', e)
       }
     },
-    async getAvatar(id) {
-      const fetchUrl = import.meta.env.VITE_FLAPI_URL + '/user/' + id + '/avatar'
-      try {
-        console.log(fetchUrl)
-        const response = await axios.get(fetchUrl, {
-          headers: {
-            'ngrok-skip-browser-warning': 'true',
-            'Content-Type': 'application/json'
-          },
-          withCredentials: false
-        })
-        console.log(response.data)
-        this.avatar = response.data.avatarUrl
-      } catch (e) {
-        console.error('flAPI error:', e)
-      }
-    }
   }
 }
 </script>
