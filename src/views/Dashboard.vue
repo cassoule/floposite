0a1d58883f9eb4d5a6429d25b56e602190c8d90b
Milo <milo.grv@gmail.com>
2025-05-07 15:32:59 +0200

    first test flAPI

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Welcome, User {{ discordId }}</h1>
+    <h1>Salut {{ discordId }} :)</h1>
     <!-- Add your user-specific content here -->
     <button @click="logout">Logout</button>
   </div>
@@ -13,14 +13,30 @@ export default {
       discordId: null
     }
   },
-  mounted() {
+  async mounted() {
     this.discordId = localStorage.getItem('discordId');
     if (!this.discordId) this.$router.push('/');
+    await this.getUsers()
   },
   methods: {
     logout() {
       localStorage.removeItem('discordId');
       this.$router.push('/');
+    },
+    async getUsers() {
+      const fetchUrl = process.env.FLAPI_URL + '/users'
+      const response = await fetch(fetchUrl)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Error fetching flAPI users')
+          }
+          return response.json()
+        })
+        .catch(err => {
+          console.error('Error with flAPI:', err)
+        })
+      console.log('flAPI users :')
+      console.log(response)
     }
   }
 }
