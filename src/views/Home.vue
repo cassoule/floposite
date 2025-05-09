03c05cbf0e86af6512802b58a116f57de9a93223
Milo <milo.grv@gmail.com>
2025-05-09 02:04:56 +0200

    login btn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,14 @@
   <div class="login">
     <h1 style="font-size: 3em"><span style="color: #5865F2">Flopo</span>Site</h1>
     <p>Connectes-toi via <span style="color: #5865F2">Discord</span> 👇</p>
-    <a :href="discordAuthUrl" class="discord-login">
+<!--    <a :href="discordAuthUrl" class="discord-login">
       Connexion
+    </a>-->
+    <a :href="discordAuthUrl">
+      <button class="btn-login">
+        <span>Connexion</span>
+        <div class="shine"></div>
+      </button>
     </a>
   </div>
 </template>
@@ -30,4 +36,72 @@ export default {
   display: inline-block;
   margin-top: 20px;
 }
+
+.btn-login {
+  box-sizing: border-box;
+  position: relative;
+  margin-top: 40px;
+  padding: 12px 40px;
+  font-size: 1em;
+  font-weight: 500;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+  color: #ddd;
+  background: #1c2526;
+  border-radius: 12px;
+  cursor: pointer;
+  overflow: hidden !important;
+  z-index: 1;
+  transition: all .3s ease;
+}
+
+.btn-login::before {
+  content: '';
+  position: absolute;
+  inset: -2px;
+  background: linear-gradient(45deg, #5865f2, #7984f5);
+  border-radius: 14px;
+  z-index: -1;
+  transition: all .3s ease;
+}
+
+.btn-login::after {
+  content: '';
+  position: absolute;
+  inset: 2px;
+  background: #1c2526;
+  border-radius: 10px;
+  z-index: -1;
+}
+
+.btn-login:hover {
+  transform: scale(1.03);
+}
+
+.btn-login:hover::before {
+  filter: blur(1px);
+}
+
+.btn-login:active {
+  transform: scale(.95);
+}
+
+.shine {
+  box-sizing: border-box;
+  position: absolute;
+  top: -50%;
+  left: -50%;
+  width: 200%;
+  height: 200%;
+  background: linear-gradient(120deg, transparent, rgba(255, 255, 255, 0.2), transparent);
+  transform: translateX(-100%);
+  transition: transform .5s ease;
+  z-index: 0;
+}
+
+.btn-login:hover .shine {
+  transform: translateX(100%);
+}
+
+
 </style>
