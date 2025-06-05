90817b36324e66c5cdb6d06ccf8ea7db85c5102c
Milo <milo.grv@gmail.com>
2025-06-05 14:14:53 +0200

    rooms

--- src/views/PokerHome.vue
+++ src/views/PokerHome.vue
@@ -1,10 +1,83 @@
 <script>
+import { io } from 'socket.io-client'
+import axios from 'axios'
 
+export default {
+  data() {
+    return {
+      socket: null,
+      discordId: null,
+
+      rooms: null,
+    }
+  },
+
+  async mounted() {
+    this.discordId = localStorage.getItem('discordId')
+    if (!this.discordId) this.$router.push('/')
+
+    this.initSocket()
+    await this.getRooms()
+  },
+
+  methods: {
+    initSocket() {
+      this.socket = io(import.meta.env.VITE_FLAPI_URL, {
+        withCredentials: false,
+        extraHeaders: {
+          'ngrok-skip-browser-warning': 'true',
+        },
+      })
+
+      this.socket.emit('tictactoeconnection', { id: this.discordId })
+
+      this.socket.on('new-poker-room', async () => {
+        await this.getRooms()
+      })
+    },
+
+    async createRoom() {
+      const url = import.meta.env.VITE_FLAPI_URL + '/create-poker-room'
+      try {
+        const response = await axios.post(url, { creatorId: this.discordId })
+      } catch (e) {
+        console.log(e)
+      }
+    },
+
+    async getRooms() {
+      const url = import.meta.env.VITE_FLAPI_URL + '/poker-rooms'
+      try {
+        const response = await axios.get(url)
+        this.rooms = response.data.rooms
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+}
 </script>
 
 <template>
   <v-layout>
-    <p>Poker Home</p>
+    <v-main>
+      <h1>Poker Home</h1>
+      <v-btn class="mt-4" text="Créer un lobby" variant="flat" color="primary" rounded="lg" @click="createRoom"></v-btn>
+
+      <div>
+        <v-btn
+          v-for="room in rooms"
+          :key="room.id"
+          :text="room.name"
+          class="my-2"
+          color="primary"
+          variant="tonal"
+          block
+          @click="$router.push('/poker/' + room.id)"
+        />
+      </div>
+    </v-main>
+
     <v-btn class="back-btn text-none" text="Retour" variant="tonal" color="#ddd" @click="$router.push('/dashboard')"></v-btn>
   </v-layout>
 </template>
--- src/views/PokerRoom.vue
+++ src/views/PokerRoom.vue
@@ -1,11 +1,90 @@
 <script>
+import { io } from 'socket.io-client'
+import axios from 'axios'
 
+export default {
+  data() {
+    return {
+      socket: null,
+      discordId: null,
+
+      room: null,
+      roomTimeout: false,
+    }
+  },
+
+  async mounted() {
+    this.discordId = localStorage.getItem('discordId')
+    if (!this.discordId) this.$router.push('/')
+
+    this.initSocket()
+    await this.getRoom(this.$route.params.id)
+  },
+
+  created() {
+    setTimeout(() => {
+      if (!this.room) this.roomTimeout = true
+    }, 2000)
+  },
+
+  methods: {
+    initSocket() {
+      this.socket = io(import.meta.env.VITE_FLAPI_URL, {
+        withCredentials: false,
+        extraHeaders: {
+          'ngrok-skip-browser-warning': 'true',
+        },
+      })
+
+      this.socket.emit('tictactoeconnection', { id: this.discordId })
+
+      this.socket.on('new-poker-room', async () => {
+        await this.getRooms()
+      })
+    },
+
+    async getRoom(id) {
+      const url = import.meta.env.VITE_FLAPI_URL + '/poker-rooms/' + id
+      try {
+        const response = await axios.get(url)
+        this.room = response.data.room
+      } catch (e) {
+        console.log(e)
+      }
+    },
+  },
+}
 </script>
 
 <template>
-  <p>Poker Room : {{$route.params.id}}</p>
+  <v-layout>
+    <v-main>
+      <div v-if="room">
+        <h1>Poker Room</h1>
+        <p>{{ room }}</p>
+      </div>
+      <div v-else style="display: flex; flex-direction: column; place-items: center">
+        <v-progress-circular class="mb-6" color="primary" indeterminate />
+        <p class="text-secondary">
+          {{ roomTimeout ? "Cette partie n'a pas l'air accessible" : "Connexion..." }}
+        </p>
+      </div>
+    </v-main>
+    <v-btn
+      class="back-btn text-none"
+      text="Retour"
+      variant="tonal"
+      color="#ddd"
+      @click="$router.push('/poker')"
+    ></v-btn>
+  </v-layout>
 </template>
 
 <style scoped>
-
+.back-btn {
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+  border-radius: 12px;
+}
 </style>
