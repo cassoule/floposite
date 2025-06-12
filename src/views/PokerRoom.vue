8ba022fa66a44a33149d7d24ed3e7bde66b6e078
milo <milo.grv@gmail.com>
2025-06-12 18:16:59 +0200

    some changes

--- src/views/PokerRoom.vue
+++ src/views/PokerRoom.vue
@@ -20,6 +20,8 @@ export default {
 
     this.initSocket()
     await this.getRoom()
+
+    if (this.room.host_id === this.discordId) await this.joinRoom()
   },
 
   created() {
@@ -105,17 +107,61 @@ export default {
     <v-main class="text-secondary w-100">
       <div v-if="room" class="w-100 mt-16">
         <h1 class="text-white">
-          Poker Room
+          Flopoker
           <span class="text-primary" style="font-size: 1.2rem">{{room.name}}</span>
         </h1>
-        <p class="mb-8">{{hasJoinedRoom ? 'Tu es assis à cette table' : ''}}</p>
+        <p class="mb-8">{{hasJoinedRoom ? (room.host_id === discordId ? 'Tu es l\'host de cette table' : 'Tu es assis à cette table') : ''}}</p>
         <v-btn v-if="!hasJoinedRoom" text="S'asseoir" class="text-none" color="primary" rounded="lg" @click="joinRoom" />
-        <v-btn v-else text="Quitter" class="text-none" color="error" rounded="lg" @click="leaveRoom" />
-        <p>{{room}}</p>
-        <p v-for="player in room.players" :key="player.id" class="mt-6">
-          {{player}}
-        </p>
-        <v-btn v-if="discordId === room.host_id" text="Commencer" class="text-none" color="secondary" variant="tonal" rounded="lg" @click="startGame" />
+        <v-btn v-else :text="Object.keys(room.players).length === 1 ? 'Supprimer la table' : 'Quitter'" class="text-none" color="error" rounded="lg" @click="leaveRoom" />
+<!--        <p>{{room}}</p>-->
+        <div style="position: relative">
+          <v-card class="w-100 mt-16 py-16" style="border-radius: 200px; background: #3f51b522; border: 8px solid #3f51b599;">
+            <div class="d-flex py-16" style="place-content: center">
+              <div v-for="card in room.tapis" :key="card" class="mr-2">
+                <v-img :src="'/cards/'+card+'.png'" :alt="card" width="80"></v-img>
+              </div>
+              <div v-for="card in [1, 2, 3, 4, 5]">
+                <v-img v-if="room.tapis.length < card" :src="'/cards/card_back.png'" width="80" class="mr-2"></v-img>
+              </div>
+            </div>
+          </v-card>
+          <div class="d-flex" style="gap: 1rem">
+            <v-card v-for="(player, index) in Object.values(room.players)" :key="player.id" :variant="player.id === discordId ? 'flat' : 'tonal'" class="mt-6" rounded="xl" :style="`width: 200px; ${room.current_player === player.id ? 'border: 4px solid #5865f2' : 'border: 4px solid transparent'}`">
+              <v-card-title>
+                {{player.globalName}}
+              </v-card-title>
+              <v-card-subtitle>
+                BET : {{player.bet ?? 0}} FlopoCoins
+              </v-card-subtitle>
+              <v-card-subtitle class="mb-3">
+                BANK : {{player.bank}} FlopoCoins
+              </v-card-subtitle>
+              <div v-if="room.playing">
+                <v-card-text v-if="player.id === discordId" class="d-flex" style="place-content: center; gap: 1rem">
+                  <div v-for="card in player.hand" :key="card">
+                    <v-img :src="'/cards/'+card+'.png'" :alt="card" width="70"></v-img>
+                  </div>
+                </v-card-text>
+                <v-card-text v-else class="d-flex" style="place-content: center; gap: 1rem">
+                  <div><v-img :src="'/cards/card_back.png'" alt="back_card" width="70"></v-img></div>
+                  <div><v-img :src="'/cards/card_back.png'" alt="back_card" width="70"></v-img></div>
+                </v-card-text>
+                <v-card-item>
+                  {{room.dealer === player.id ? 'Dealer' : ''}}
+                  {{room.sb === player.id ? 'Small Blind' : ''}}
+                  {{room.bb === player.id ? 'Big Blind' : ''}}
+                  <br>
+                  {{room.current_player === player.id ? 'Playing' : ''}}
+                </v-card-item>
+                <v-card-text v-if="player.id === discordId" class="text-center font-weight-bold">
+                  {{player.solve}}
+                </v-card-text>
+              </div>
+            </v-card>
+          </div>
+        </div>
+
+        <v-btn v-if="discordId === room.host_id && !room.playing" text="Commencer" class="text-none mt-8" color="secondary" variant="tonal" rounded="lg" :disabled="Object.keys(room.players).length < 2" @click="startGame" />
       </div>
 
       <div v-else class="w-100 mt-16" style="display: flex; flex-direction: column; place-items: center">
