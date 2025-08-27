fb27d62d36f9651f29475d16fd2dda01471c2a19
milo <milo.grv@gmail.com>
2025-08-27 17:34:55 +0200

    inventory in akhy profile and video

--- src/views/AkhyStats.vue
+++ src/views/AkhyStats.vue
@@ -4,6 +4,8 @@ import axios from 'axios'
 export default {
   name: 'AkhyStats',
 
+  components: {},
+
   data() {
     return {
       users: null,
@@ -12,6 +14,7 @@ export default {
       elo: null,
       elo_graph: null,
       user_inventory: null,
+      skinsVideoUrls: {},
       active_slowmodes: null,
       user_isTimedOut: false,
       games: null,
@@ -28,6 +31,7 @@ export default {
       this.elo = await this.getElo(id)
       this.elo_graph = await this.getEloGraph(id)
       await this.getInventory()
+      this.fetchSkinsVideoUrls()
       await this.getActiveSlowmodes()
       await this.isTimedOut()
       await this.getGames()
@@ -123,6 +127,26 @@ export default {
           withCredentials: false,
         })
         this.user_inventory = response.data.inventory
+      } catch (e) {
+        console.error('flAPI error:', e)
+      }
+    },
+
+    fetchSkinsVideoUrls() {
+      this.user_inventory.forEach(async (s) => {
+        console.log(s)
+        await this.getSkinVideoUrl(s)
+      })
+    },
+
+    async getSkinVideoUrl(skin) {
+      const fetchUrl = import.meta.env.VITE_FLAPI_URL + '/skin/' + skin.uuid
+      try {
+        const response = await axios.post(fetchUrl, {
+          level: skin.currentLvl,
+          chroma: skin.currentChroma,
+        });
+        this.skinsVideoUrls[skin.uuid] = response.data.url
       } catch (e) {
         console.error('flAPI error:', e)
       }
@@ -444,11 +468,112 @@ export default {
             <p class="text-center pt-12 pb-16">Aucune partie classée</p>
           </v-list-item>
         </v-list>
+
+        <v-list
+          v-if="user_inventory"
+          width="100%"
+          class="mt-10 py-0 position-relative"
+          rounded="xl"
+          bg-color="#181818"
+          variant="tonal"
+          style="border: 2px solid #ffffff55; max-height: 500px"
+        >
+          <v-list-item
+            class="pt-4 position-sticky top-0 w-100"
+            rounded="0"
+            style="backdrop-filter: blur(5px); z-index: 2"
+          >
+            <h2>Inventaire</h2>
+          </v-list-item>
+          <v-list-item
+            v-if="user_inventory.length > 0"
+            class="pb-3"
+            style="z-index: 1"
+          >
+            <div class="inventory">
+              <div
+                v-for="skin in user_inventory"
+                :key="skin.id"
+                class="inventory-item"
+                :style="`border-radius: 10px;`"
+              >
+                <div
+                  style="
+                display: flex;
+                place-content: space-between;
+                min-width: 300px;
+                width: 100%;
+                padding: 0.5em 1em;
+              "
+                >
+                  <div style="display: flex; width: 70%; gap: 1em">
+                    <v-img
+                      :src="skin.displayIcon"
+                      class="skin-img"
+                      height="25"
+                      min-width="70"
+                      max-width="70"
+                    />
+                    <span
+                      style="
+                    color: #ddd;
+                    overflow: hidden;
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
+                  "
+                    >{{ skin.displayName }}</span
+                    >
+                  </div>
+
+                  <span style="font-weight: bold">{{ skin.currentPrice.toFixed(2) }}€</span>
+                  <v-menu v-if="skinsVideoUrls[skin.uuid] !== null" activator="parent" location="center" open-on-click close-on-content-click transition="scale-transition">
+                    <v-list
+                      width="400"
+                      class="mr-2 py-0"
+                      elevation="20"
+                      rounded="xl"
+                      bg-color="#181818"
+                      base-color="white"
+                      variant="tonal"
+                      :style="`border: 2px solid #${skin.tierColor}`"
+                    >
+                      <v-list-item class="pa-0">
+                        <v-video
+                          class="skin-video w-100"
+                          :volume="25"
+                          theme="dark"
+                          color="primary"
+                          autoplay
+                          floating
+                          controls-variant="hidden"
+                          elevation="0"
+                          :src="skinsVideoUrls[skin.uuid]"
+                        ></v-video>
+                      </v-list-item>
+                      <div style="position: absolute; top: 10px; right: 10px; cursor: pointer">
+                        <v-icon class="mdi mdi-close video-close-icon"/>
+                      </div>
+                    </v-list>
+                  </v-menu>
+                </div>
+                <div
+                  class="skin-bg"
+                  :style="`background: radial-gradient(circle at -50% 0%, #${skin.tierColor}, transparent 80%)`"
+                ></div>
+              </div>
+            </div>
+          </v-list-item>
+          <v-list-item v-else>
+            <p class="text-center pt-12 pb-16">Aucun skin dans l'inventaire</p>
+          </v-list-item>
+        </v-list>
       </div>
 
       <div v-else>
         <p>Joueur Introuvable</p>
       </div>
+
+
     </v-main>
 
     <v-btn
@@ -475,6 +600,19 @@ export default {
 
 .graphs {
   width: 50%;
+}
+
+.video-close-icon {
+  z-index: 1 !important;
+}
+
+.video-close-icon::after {
+  content: '';
+  position: absolute;
+  z-index: -1;
+  width: 0;
+  height: 0;
+  box-shadow: 0 0 10px 10px gray;
 }
 
 .win-card {
